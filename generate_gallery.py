f582c6166c5f4ec2e6127f9140005068c0ed4430
Chainan Butkamwong <chainun_06@hotmail.com>
2024-10-25 04:42:24 +0700

    add icon

--- generate_gallery.py
+++ generate_gallery.py
@@ -21,6 +21,7 @@ html_template = """
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Nikkes Arena</title>
+    <link rel="icon" href="image/favicon.ico" type="image/x-icon">
     <style>
         body {
             background-color: #000;  /* Black background */
